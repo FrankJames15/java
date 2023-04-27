75922dc519f7d061dda99a4d98c15a6f4dc0cb2c
Frank James <106287785+FrankJames15@users.noreply.github.com>
2023-04-27 08:47:32 +0000

    1

--- test.java
+++ test.java
@@ -1,1 +1,17 @@
+import java.io.*;
 
+public class test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // program that define odd and even number
+        System.out.print("Enter a number: ");
+        int num = Integer.parseInt(br.readLine());
+        if (num % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+
+        
+    }
+}
